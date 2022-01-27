67eee46adb0e42a3e7509b90dcd9e63a2a8467e2
Javier Diaz <javier.diaz.palacios@gmail.com>
2022-01-27 01:26:51 -0300

    Create controller for the interaction between Stores and Products
    - List the products in a store
    - Add a product to a store
    - Remove a product from a store

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :stores, :products
+  resources :stores do
+    resources :products, controller: 'stores_products', only: %i[index update destroy]
+  end
+  resources :products
 end
