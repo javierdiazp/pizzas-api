7ed54fd762c8b0463a96e41f40728edfabeb5d9e
Javier Diaz <javier.diaz.palacios@gmail.com>
2022-01-27 01:20:00 -0300

    Create a 'has_and_belongs_to_many' association between stores and products

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,4 @@
 class Store < ApplicationRecord
   validates :name, :address, :email, :phone, presence: true
+  has_and_belongs_to_many :products
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_27_040555) do
+ActiveRecord::Schema.define(version: 2022_01_27_041816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2022_01_27_040555) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products_stores", id: false, force: :cascade do |t|
+    t.bigint "store_id", null: false
+    t.bigint "product_id", null: false
+    t.index ["store_id", "product_id"], name: "index_products_stores_on_store_id_and_product_id", unique: true
   end
 
   create_table "stores", force: :cascade do |t|
