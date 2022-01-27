05423569578f275c134c34f572f1e6b23e96efef
Javier Diaz <javier.diaz.palacios@gmail.com>
2022-01-27 01:15:47 -0300

    Add tests that validate routes and controller actions of Products

--- spec/requests/product_spec.rb
+++ spec/requests/product_spec.rb
@@ -1,5 +1,133 @@
 require 'rails_helper'
 
 RSpec.describe 'Products API', type: :request do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:products) { create_list(:product, 10) }
+  let(:product_id) { products.first.id }
+  let(:valid_attributes) { attributes_for(:product) }
+
+  describe 'GET /products' do
+    before { get '/products' }
+
+    it 'returns products' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'GET /products/:id' do
+    before { get "/products/#{product_id}" }
+
+    context 'when the record exists' do
+      it 'returns the product' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(product_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:product_id) { 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Product/)
+      end
+    end
+  end
+
+  describe 'POST /products' do
+    context 'when the request is valid' do
+      before { post '/products', params: valid_attributes }
+
+      it 'creates a product' do
+        expect(json['name']).to eq(valid_attributes[:name])
+        expect(json['sku']).to eq(valid_attributes[:sku])
+        expect(json['type']).to eq(valid_attributes[:type])
+        expect(json['price']).to eq(valid_attributes[:price])
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context 'when the request is invalid' do
+      let(:invalid_attributes) { valid_attributes.reject { |k| k == :name } }
+
+      before { post '/products', params: invalid_attributes }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match(/Validation failed: Name can't be blank/)
+      end
+    end
+  end
+
+  describe 'PUT /products/:id' do
+    let(:partial_attributes) { valid_attributes.slice(:name, :sku) }
+
+    before { put "/products/#{product_id}", params: partial_attributes }
+
+    context 'when the record exists' do
+      it 'updates the record' do
+        expect(response.body).to be_empty
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(:no_content)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:product_id) { 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Product/)
+      end
+    end
+  end
+
+  describe 'DELETE /products/:id' do
+    before { delete "/products/#{product_id}" }
+
+    context 'when the record exists' do
+      it 'deletes the record' do
+        expect(response.body).to be_empty
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(:no_content)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:product_id) { 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Product/)
+      end
+    end
+  end
 end
