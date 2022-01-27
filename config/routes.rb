cb137d662b7846c777fa867b291f667fbd6403f9
Javier Diaz <javier.diaz.palacios@gmail.com>
2022-01-27 02:05:52 -0300

    Create controller for listing all Products in an Order

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'orders#index'
+  resources :orders do
+    resources :products, controller: 'orders_products', only: %i[index]
+  end
   resources :stores do
     resources :products, controller: 'stores_products', only: %i[index update destroy]
   end
-  resources :orders, :products
+  resources :products
 end
